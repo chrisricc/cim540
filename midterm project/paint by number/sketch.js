27c56a63935b0397c887c1bfe968c7745a38e645
Christina Riccardi <car252@miami.edu>
2018-03-22 11:05:18 -0400

    go

--- midterm project/paint by number/sketch.js
+++ midterm project/paint by number/sketch.js
@@ -4,12 +4,12 @@ var brushHeight = 10;
 
 var buttonRedColor, buttonGreenColor, buttonBlueColor;
 
+function preload () {
+  bgimage = loadImage("assets/plane.jpg")
+}
 
 function setup() {
-
-
     createCanvas(800, 400);
-    bg = loadImage("assets/plane.jpg");
 
     interfaceItems.push(new interface(50, 10, 40, color(0, 255, 0)));
     interfaceItems.push(new interface(100, 10, 40, color(255, 0, 0)));
